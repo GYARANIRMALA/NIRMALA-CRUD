e8bcd780e27a385bd33c1232afd6b342ffa52d20
unknown <gyaranirmala@gmail.com>
2022-08-12 17:14:53 +0530

    order modified

--- task1_app/views.py
+++ task1_app/views.py
@@ -9,33 +9,36 @@ class BlogApi(viewsets.ViewSet):
 
     def create(self, request, *args, **kwargs):
         try:
-            # if request.data["status"] not in [Blog.StatusTypes.draft,Blog.StatusTypes.published,Blog.StatusTypes.pending]:
-            #     return Response({"error":"Invalid status"},status=status.HTTP_400_BAD_REQUEST)
+            if request.data["status"] not in [Blog.StatusTypes.draft,Blog.StatusTypes.published,Blog.StatusTypes.pending]:
+                return Response({"error":"Invalid status"},status=status.HTTP_400_BAD_REQUEST)
 
-            # blog = Blog(
-            #     title = request.data["title"],
-            #     description = request.data["description"],
-            #     primary_image = request.data["primary_image"],
-            #     likes = request.data["likes"],
-            #     views = request.data["views"],
-            #     status = request.data["status"],
-            #     active = request.data["active"],
-            #     order = request.data["order"],
-            #     created_by = request.data["created_by"],
-            #     updated_by = request.data["updated_by"],
-            # )
-            # blog.save()
-            # return Response(
-            #     BlogSerializer(blog).data, status=status.HTTP_201_CREATED
-            # )
+            if request.data["order"] < 0 :
+                return Response({"error": "Order should not be negitive"}, status=status.HTTP_400_BAD_REQUEST)
 
-            serializer = BlogSerializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(
-                    serializer.data, status=status.HTTP_201_CREATED
-                )
-            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            blog = Blog(
+                title = request.data["title"],
+                description = request.data["description"],
+                primary_image = request.data["primary_image"],
+                likes = request.data["likes"],
+                views = request.data["views"],
+                status = request.data["status"],
+                active = request.data["active"],
+                order = request.data["order"],
+                created_by = request.data["created_by"],
+                updated_by = request.data["updated_by"],
+            )
+            blog.save()
+            return Response(
+                BlogSerializer(blog).data, status=status.HTTP_201_CREATED
+            )
+
+            # serializer = BlogSerializer(data=request.data)
+            # if serializer.is_valid():
+            #     serializer.save()
+            #     return Response(
+            #         serializer.data, status=status.HTTP_201_CREATED
+            #     )
+            # return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
         except Exception as err:
             print("error BlogApis create",err)
             return Response({"error": str(err)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
